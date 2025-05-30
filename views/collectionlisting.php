75f8d447a254a36885d48e66b4017dfe3d37142c
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-05-30 12:28:15 +0200

    correct escaping
    correct escaping

--- views/collectionlisting.php
+++ views/collectionlisting.php
@@ -1,33 +1,41 @@
-<aside id="<?php echo "collection-{$collection->slug}" ?>" class="also-in-this-collection">
-<?php if( $title ) : ?>
-<div class="collection-title">
-	<<?php echo $titlewrap ?>><?php echo $title ?></<?php echo $titlewrap ?>>
-</div>
-<?php endif ?>
-<?php if( $description ) : ?>
-	<div class="collection-description"><?php echo $description ?></div>
-<?php endif ?>
+<aside id="<?php echo esc_attr( "collection-{$collection->slug}" ); ?>" class="also-in-this-collection">
 
-<?php if( !$hidecollectionlisting ) : ?>
-	<ol start="<?php echo $logicalframe[0] ?>" <?php echo $sortorder === 'desc' ? 'reversed' : '' ?>>
-	<?php foreach( $collectionposts as $index => $collectionpost ) : ?>
-		<?php if( !is_single() || $collectionpost->ID !== $post->ID ) : ?>
-			<li class="collection-post">
-				<a href="<?php echo get_permalink( $collectionpost->ID ) ?>"
-				><?php echo get_the_title( $collectionpost->ID ) ?></a>
-			</li>
-		<?php else : ?>
-			<li class="collection-post current">
-				<strong><?php echo get_the_title( $collectionpost->ID ) ?></strong>
-			</li>
-		<?php endif ?>
-	<?php endforeach ?>
-	</ol>
-<?php endif ?>
+<?php if ( $title ) : ?>
+    <div class="collection-title">
+        <<?php echo esc_html( $titlewrap ); ?>>
+            <?php echo esc_html( $title ); ?>
+        </<?php echo esc_html( $titlewrap ); ?>>
+    </div>
+<?php endif; ?>
 
-<?php if( $hidecollectionlisting || $framing || $alwayslinkcollection ) : ?>
-<div class="collection-link">
-	<a href="<?php echo get_term_link( $collection ) ?>"><?php _e( 'View the entire collection', 'also-in-this-collection' ) ?></a>
-</div>
-<?php endif ?>
+<?php if ( $description ) : ?>
+    <div class="collection-description"><?php echo wp_kses_post( $description ); ?></div>
+<?php endif; ?>
+
+<?php if ( ! $hidecollectionlisting ) : ?>
+    <ol start="<?php echo esc_attr( $logicalframe[0] ); ?>" <?php echo ( $sortorder === 'desc' ) ? 'reversed' : ''; ?>>
+        <?php foreach ( $collectionposts as $index => $collectionpost ) : ?>
+            <?php if ( ! is_single() || $collectionpost->ID !== $post->ID ) : ?>
+                <li class="collection-post">
+                    <a href="<?php echo esc_url( get_permalink( $collectionpost->ID ) ); ?>">
+                        <?php echo esc_html( get_the_title( $collectionpost->ID ) ); ?>
+                    </a>
+                </li>
+            <?php else : ?>
+                <li class="collection-post current">
+                    <strong><?php echo esc_html( get_the_title( $collectionpost->ID ) ); ?></strong>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ol>
+<?php endif; ?>
+
+<?php if ( $hidecollectionlisting || $framing || $alwayslinkcollection ) : ?>
+    <div class="collection-link">
+        <a href="<?php echo esc_url( get_term_link( $collection ) ); ?>">
+            <?php echo esc_html__( 'View the entire collection', 'also-in-this-collection' ); ?>
+        </a>
+    </div>
+<?php endif; ?>
+
 </aside>
