bf6336aae7038e8cc82348fc0b3df13ef111f5dd
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-05-30 12:31:03 +0200

    escaped correctly
    escaped correctly

--- views/admin/settings.php
+++ views/admin/settings.php
@@ -1,9 +1,9 @@
 <div class="wrap">
-	<div id="icon-themes" class="icon32"></div>
-	<h2><?php _e( 'Also In This Collection Settings', 'also-in-this-collection' ) ?></h2>
-	<form method="post" action="options.php">
-	<?php settings_fields( \elicawebservices\wordpress\also_in_this_collection\COLLECTION_SLUG ) ?>
-	<?php do_settings_sections( \elicawebservices\wordpress\also_in_this_collection\COLLECTION_SETTINGS_PAGE ) ?>    
-	<?php submit_button() ?>
-	</form>
+    <div id="icon-themes" class="icon32"></div>
+    <h2><?php esc_html_e( 'Also In This Collection Settings', 'also-in-this-collection' ); ?></h2>
+    <form method="post" action="options.php">
+        <?php settings_fields( \elicawebservices\wordpress\also_in_this_collection\COLLECTION_SLUG ); ?>
+        <?php do_settings_sections( \elicawebservices\wordpress\also_in_this_collection\COLLECTION_SETTINGS_PAGE ); ?>    
+        <?php submit_button(); ?>
+    </form>
 </div>
