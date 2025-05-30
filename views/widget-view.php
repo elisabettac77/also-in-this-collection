9a227c320b6b8f5d05e8243b73ecdb1ea453ff71
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-05-30 12:26:52 +0200

    escaped widget output
    escaped widget output

--- views/widget-view.php
+++ views/widget-view.php
@@ -1,7 +1,7 @@
 <div class="also-in-this-collection-widget">
-	<?php echo $widget['before_widget'] ?>
+    <?php echo wp_kses_post( $widget['before_widget'] ); ?>
 
-	<?php echo $collectionlisting ?>
+    <?php echo wp_kses_post( $collectionlisting ); ?>
 
-	<?php echo $widget['after_widget'] ?>
+    <?php echo wp_kses_post( $widget['after_widget'] ); ?>
 </div>
