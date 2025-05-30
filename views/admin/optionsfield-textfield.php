75037e2fb26dc9f09b337b76b045925ac2471d18
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-05-30 12:32:04 +0200

    corrected escaping
    corrected escaping

--- views/admin/optionsfield-textfield.php
+++ views/admin/optionsfield-textfield.php
@@ -1,5 +1,10 @@
-<input type="text" id="<?php echo $id ?>" name="<?php echo $name ?>" value="<?php echo $value ?>"/>
+<input 
+    type="text" 
+    id="<?php echo esc_attr( $id ); ?>" 
+    name="<?php echo esc_attr( $name ); ?>" 
+    value="<?php echo esc_attr( $value ); ?>" 
+/>
 
-<?php if( isset( $label ) ) : ?>
-<label for="<?php echo $id ?>"><?php echo $label ?></label>
-<?php endif ?>
+<?php if ( isset( $label ) ) : ?>
+    <label for="<?php echo esc_attr( $id ); ?>"><?php echo esc_html( $label ); ?></label>
+<?php endif; ?>
