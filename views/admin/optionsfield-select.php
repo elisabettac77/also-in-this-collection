ed6c5635594c2ad1047863967c93ffed91523d1d
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-05-30 12:29:39 +0200

    Escaping corrected
    escaping corrected

--- views/admin/optionsfield-select.php
+++ views/admin/optionsfield-select.php
@@ -1,5 +1,7 @@
-<select id="<?php echo $id ?>" name="<?php echo $name ?>">
-<?php foreach( $values as $index => $value ) : ?>
-	<option value="<?php echo $value ?>" <?php echo $selected[$index] ?>><?php echo isset( $labels[$index] ) ? $labels[$index] : '' ?></option>
-<?php endforeach ?>
+<select id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $name ); ?>">
+    <?php foreach ( $values as $index => $value ) : ?>
+        <option value="<?php echo esc_attr( $value ); ?>" <?php echo isset( $selected[ $index ] ) ? esc_attr( $selected[ $index ] ) : ''; ?>>
+            <?php echo isset( $labels[ $index ] ) ? esc_html( $labels[ $index ] ) : ''; ?>
+        </option>
+    <?php endforeach; ?>
 </select>
